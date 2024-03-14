4c7991866977abbc93b5fe66aee7a4ce3dcb620e
doramaku <119169567+doramaku@users.noreply.github.com>
2024-03-14 14:13:52 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,165 @@
-# wa-intip
+<a name="readme-top"></a>
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/hasyim404/wa-intip/blob/master/temp/suika.png">
+    <img style="border-radius: 50%;" src="temp/suika.png" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">WA-Intip</h3>
+
+  <p align="center">
+    Sebuah potongan kode dari SuikaBot Project untuk melihat atau bypass pesan 1x Lihat dari pengguna lain dan dikirim ke nomor kita sendiri.
+    <br />
+    <a href="https://wa.me/6285179910233"><strong>Try our sticker bot »</strong></a>
+    <br />
+    <br />
+    Terimakasih kepada:
+    <br />
+    <a href="https://github.com/xhaeffer">@xhaeffer</a>
+    ·
+    <a href="https://github.com/iegl3">@iegl3 </a>
+  </p>
+</div>
+
+<!-- ABOUT THE PROJECT -->
+
+## About WA-Intip
+
+### [*] Private Chat:
+
+<div align="center">
+    <a href="https://github.com/hasyim404/wa-intip/blob/master/assets/img/private-msg.png">
+    <img style="border-radius: 1%" src="assets/img/private-msg.png">
+    </a>
+</div>
+
+### [*] Only Admin can send msg in Group:
+
+<div align="center">
+    <a href="https://github.com/hasyim404/wa-intip/blob/master/assets/img/only-admin-can-send-msg-group.png">
+    <img style="border-radius: 1%" src="assets/img/only-admin-can-send-msg-group.png">
+    </a>
+</div>
+
+## How to Run
+
+### <ins> Windows </ins>
+
+**Note:** RDP belum tested !! </br>
+
+<!-- **Note:** Kalo error coba ganti path **../chrome.exe** nya lalu sesuaikan sama punya lu. -->
+
+1. Download & Install [Node.JS](https://nodejs.org/en), kalo udah install lanjut ke next step.
+
+2. Download atau Clone repository ini:
+
+   ```sh
+   git clone https://github.com/hasyim404/wa-intip.git
+   ```
+
+3. Masuk ke folder project:
+
+   ```sh
+   cd WA-Intip
+   ```
+
+4. Install NPM packages:
+
+   ```sh
+   npm install
+   ```
+
+5. Jalanin program:
+
+   ```sh
+   npm run start
+   ```
+
+   atau:
+
+   ```sh
+   node index.js
+   ```
+
+6. Tunggu QR-Code muncul, lalu scan pake **no WA utama lu**
+
+7. Setelah muncul **"Client is Ready!"** maka sudah siap digunakan, enjoy.
+
+### <ins>Linux</ins>
+
+**Note:** VPS belum tested !! </br>
+
+<!-- - _Coming soon_ -->
+
+1. Install Curl
+
+   ```sh
+   sudo apt-get install curl
+   ```
+
+2. Download NodeJS (v20.x)
+
+   ```sh
+   curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+   ```
+
+3. Install NodeJS & NPM
+
+   ```sh
+   sudo apt-get install nodejs
+   ```
+
+4. Install Chromium-Browser
+
+   ```sh
+   sudo apt-get install chromium-browser
+   ```
+
+5. Download atau Clone repository ini:
+
+   ```sh
+   git clone https://github.com/hasyim404/wa-intip.git
+   ```
+
+6. Masuk ke folder project:
+
+   ```sh
+   cd WA-Intip
+   ```
+
+7. Install NPM packages:
+
+   ```sh
+   npm install
+   ```
+
+8. Jalanin program:
+
+   ```sh
+   npm run start
+   ```
+
+   atau:
+
+   ```sh
+   node index.js
+   ```
+
+9. Tunggu QR-Code muncul, lalu scan pake **no WA utama lu**
+
+10. Setelah muncul **"Client is Ready!"** maka sudah siap digunakan, enjoy.
+
+## Available Commands
+
+- `.intip` = Reply pesan 1x lihat, trus ketik `.intip`, send.
+
+  ```sh
+  Langsung eksekusi di nomer target (percakapan saat ini).
+  ```
+
+- `aptuh` = Reply pesan 1x lihat, trus ketik `aptuh`, send.
+  ```sh
+  Dikirim ke nomor sendiri (elu)
+  ```
---
+++ index.js
@@ -0,0 +1,209 @@
+const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+
+const mime = require("mime-types");
+const fs = require("fs");
+
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+const unlinkAsync = util.promisify(fs.unlink);
+
+const osPlatform = require("os").platform();
+console.log("Running on platform: ", osPlatform);
+
+// FFmpeg
+const ffmpeg = require("fluent-ffmpeg");
+const ffprobe = require("@ffprobe-installer/ffprobe");
+const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
+const ffmpegs = require("ffmpeg-static");
+ffmpeg.setFfprobePath(ffprobe.path);
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+
+// Puppeter path
+let execPath;
+if (/^win/i.test(osPlatform)) {
+  // Sesuaikan dengan lokasi Chrome terinstall di windows lu
+  execPath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+} else if (/^linux/i.test(osPlatform)) {
+  // Sesuaikan path chromium-browser di linux, cek path dengan perintah "which chromium-browser"
+  execPath = "/usr/bin/chromium-browser";
+}
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  ffmpegPath: ffmpegs,
+  puppeteer: {
+    executablePath: execPath,
+  },
+});
+
+const messageQueue = [];
+let isProcessingQueue = false;
+const timeer = 1000;
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("loading_screen", (percent, message) => {
+  console.log("Loading:", percent, message);
+});
+
+client.on("ready", async () => {
+  console.log("\nClient is ready!\n");
+});
+
+client.on("message_create", async (message) => {
+  try {
+    const body = message.body.toLowerCase();
+    const targetNum = message._data.id.fromMe;
+
+    if (targetNum === true) {
+      messageQueue.push(message);
+
+      if (!isProcessingQueue) {
+        processMessageQueue();
+      }
+    } else {
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
+
+client.initialize();
+
+async function processMessageQueue() {
+  isProcessingQueue = true;
+
+  while (messageQueue.length > 0) {
+    const message = messageQueue.shift();
+    const body = message.body.toLowerCase();
+
+    switch (true) {
+      case [".intip"].includes(body) && message.hasQuotedMsg:
+        await delay(timeer);
+        const quotedMessageIntip = await message.getQuotedMessage();
+
+        if (
+          quotedMessageIntip &&
+          quotedMessageIntip._data.isViewOnce === true
+        ) {
+          await intipMessage(quotedMessageIntip);
+        } else {
+          console.log("Hanya berlaku untuk pesan 1x lihat");
+        }
+        break;
+
+      case ["aptuh"].includes(body) && message.hasQuotedMsg:
+        await delay(timeer);
+        const quotedMessageIntipSend = await message.getQuotedMessage();
+        const targetNum = quotedMessageIntipSend.to;
+
+        if (
+          quotedMessageIntipSend &&
+          quotedMessageIntipSend._data.isViewOnce === true
+        ) {
+          await intipMessageSend(quotedMessageIntipSend, targetNum);
+        } else {
+          console.log("Hanya berlaku untuk pesan 1x lihat");
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+  if (messageQueue.length > 0) {
+    await processMessageQueue();
+  }
+  isProcessingQueue = false;
+}
+
+async function intipMessage(message) {
+  const media = await message.downloadMedia();
+
+  if (["video"].includes(message.type)) {
+    await mediaToMp4(media.data, media.mimetype);
+
+    const output = MessageMedia.fromFilePath("temp/output.mp4");
+
+    await delay(timeer);
+    await message.reply(output, undefined, {
+      caption: message.body ? `${message.body}` : null,
+    });
+    fs.unlinkSync("temp/output.mp4");
+    console.log("[*] Copy Video Success");
+  } else {
+    await delay(timeer);
+    message.reply(media, undefined, {
+      caption: message.body ? `${message.body}` : null,
+    });
+    console.log("[*] Copy Img Success");
+  }
+}
+
+async function intipMessageSend(message, targetNum) {
+  const media = await message.downloadMedia();
+
+  if (["video"].includes(message.type)) {
+    await mediaToMp4(media.data, media.mimetype);
+
+    const output = MessageMedia.fromFilePath("temp/output.mp4");
+
+    await delay(timeer);
+    await client.sendMessage(targetNum, output, {
+      caption: message.body ? `${message.body}` : null,
+    });
+    fs.unlinkSync("temp/output.mp4");
+    console.log("[*] Copy Video Success & send to: +" + targetNum);
+  } else {
+    await delay(timeer);
+    await client.sendMessage(targetNum, media, {
+      caption: message.body ? `${message.body}` : null,
+    });
+
+    console.log("[*] Copy Img Success & send to: +" + targetNum);
+  }
+}
+
+// Utils
+async function mediaToMp4(base64, mimeType) {
+  try {
+    const buffer = Buffer.from(base64, "base64");
+    const extension = mime.extension(mimeType) || "unknown";
+    const inputFileName = "temp/" + `input.${extension}`;
+    const outputFileName = "temp/" + `output.mp4`;
+
+    await writeFileAsync(inputFileName, buffer);
+
+    await new Promise((resolve, reject) => {
+      ffmpeg()
+        .input(inputFileName)
+        .outputOptions([
+          "-pix_fmt yuv420p",
+          "-c:v libx264",
+          "-movflags +faststart",
+          "-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'",
+        ])
+        .output(outputFileName)
+        .on("end", () => {
+          resolve();
+        })
+        .on("error", (err) => {
+          console.error("Error:", err);
+          reject(err);
+        })
+        .run();
+    });
+
+    await unlinkAsync(inputFileName);
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+}
+
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
